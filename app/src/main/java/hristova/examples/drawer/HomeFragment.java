f9d206a001ba9786bbd102dbb7efd89ccda45d48
Radostina Hristova <rhristova@despark.com>
2015-01-24 16:02:18 +0200

    Float animation of image view

--- app/src/main/java/hristova/examples/drawer/HomeFragment.java
+++ app/src/main/java/hristova/examples/drawer/HomeFragment.java
@@ -6,6 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import hristova.examples.drawer.pager.PagerAdapter;
 import hristova.examples.drawer.pager.VerticalViewPager;
@@ -24,6 +27,7 @@ public class HomeFragment extends Fragment {
     private static final int NUM_PAGES = 5;
     private VerticalViewPager viewPager;
     private PagerAdapter pagerAdapter;
+    private ImageView arrowImageView;
 
 
     public static HomeFragment newInstance() {
@@ -38,11 +42,31 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
         viewPager = (VerticalViewPager) rootView.findViewById(R.id.pager);
         pagerAdapter = new PagerAdapter(getFragmentManager(), NUM_PAGES);
         viewPager.setAdapter(pagerAdapter);
         viewPager.setPageTransformer(true, new ZoomOutPageTransformer());
+
+        arrowImageView = (ImageView) rootView.findViewById(R.id.imageView);
+
         return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        Animation floatAnimation = AnimationUtils.loadAnimation(getActivity(), R.anim.float_animation);
+        floatAnimation.setRepeatCount(Animation.INFINITE);
+        arrowImageView.startAnimation(floatAnimation);
+    }
+
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        arrowImageView.clearAnimation();
     }
 
     @Override
